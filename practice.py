069642a1450aec68acfc6eb9254fd20ad442c8b6
jaycariaga <jay_jay_xray@yahoo.com>
2019-07-31 15:33:47 -0400

    add glob tests

---
+++ practice.py
@@ -0,0 +1,21 @@
+
+import glob
+
+
+
+#for x in range(100): #goes through certain number of images
+filenames = glob.glob('/home/igolgi/snap/skype/common/Downloads/test_images/*.jpg')
+
+#files = glob.glob('./*')
+
+print(len(filenames))
+
+filenames.sort()
+print(filenames)
+
+for x in range(10):
+  print(filenames[x])
+#print(filenames)
+#print(files)
+
+
